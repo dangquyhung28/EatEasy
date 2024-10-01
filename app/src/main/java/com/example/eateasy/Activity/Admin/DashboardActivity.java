5d7a6e75069a81fc413bda43c990f5b92099b22a
DQHung <quyhung0121@gmail.com>
2024-10-02 00:28:33 +0700

    UI login and singup

--- app/src/main/java/com/example/eateasy/Activity/Admin/DashboardActivity.java
+++ app/src/main/java/com/example/eateasy/Activity/Admin/DashboardActivity.java
@@ -1,4 +1,4 @@
-package com.example.eateasy.Activity;
+package com.example.eateasy.Activity.Admin;
 
 import android.os.Bundle;
 
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.eateasy.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_dashboard);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/eateasy/Activity/User/HomeActivity.java
@@ -0,0 +1,45 @@
+package com.example.eateasy.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.eateasy.R;
+
+public class HomeActivity extends AppCompatActivity {
+    ImageView btnLogout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //ánh xá
+        initwiget();
+
+        //logout
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inten = new Intent(HomeActivity.this, Login_Activity.class);
+                startActivity(inten);
+            }
+        });
+    }
+
+    private void initwiget() {
+        btnLogout = findViewById(R.id.logoutBtn);
+    }
+}
